3821339476142274dd6f5782aea4c2da2dfe38c4
shouko <h999342@gmail.com>
2015-12-29 18:15:44 +0800

    add qa

--- views/list.php
+++ views/list.php
@@ -3,18 +3,23 @@
   foreach($data as $section) {
     echo '<h1 class="title"><span>'.$section['title'].'</span></h1>';
     switch($section['type']) {
-      case 'news':
+      case 'list':
         echo '<ul class="list">';
         echo '<li class="first">(請點擊圖示開啟相關附檔/連結)</li>';
-        foreach($section['content'] as $entry) {
-          echo '<li>';
-          if(isset($entry['cat'])) {
-            '<span'. (isset($entry['highlight']) ? ' style="background:red;color:yellow; "' : '') .'>'.$entry['cat'].'</span> '.$entry['date'].'<br />';
+        if(isset($section['content'])) {
+          foreach($section['content'] as $entry) {
+            echo '<li>';
+            if(isset($entry['cat'])) {
+              echo '<span'. (isset($entry['highlight']) ? ' style="background:red;color:yellow; "' : '') .'>'.$entry['cat'].'</span> ';
+            }
+            if(isset($entry['date'])) {
+              echo $entry['date'].'<br />';
+            }
+            foreach($entry['files'] as $file) {
+              echo $file[0].'&nbsp;<a href="'.$file[1].'"><img class="no" src="'.ROOT_URI.'assets/img/'.$file[2].'.gif" border="0" height="22" width="22" /></a>&nbsp';
+            }
+            echo '</li>';
           }
-          foreach($entry['files'] as $file) {
-            echo $file[0].'&nbsp;<a href="'.$file[1].'"><img class="no" src="'.ROOT_URI.'assets/img/'.$file[2].'.gif" border="0" height="22" width="22" /></a>&nbsp';
-          }
-          echo '</li>';
         }
         echo '</ul>';
         break;
