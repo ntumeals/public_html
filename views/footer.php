c45c12d13c1893a5dbbab3b3aa9e5c04544ef609
shouko <h999342@gmail.com>
2015-12-30 17:10:40 +0800

    add GA

--- views/footer.php
+++ views/footer.php
@@ -4,4 +4,13 @@
     地址：(10617)台北市大安區羅斯福路四段一號<br>
     E-Mail：<a href="mailto:ntumeals@ntu.edu.tw">ntumeals@ntu.edu.tw</a></center>
 </div>
-</body></html>
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-45786123-6', 'auto');
+  ga('send', 'pageview');
+
+</script></body></html>
