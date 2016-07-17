848b84b68b455880648cc78eac6d9f1044d4a25f
shouko <imshouko@gmail.com>
2016-07-18 02:16:39 +0800

    update checksys link

--- views/main.php
+++ views/main.php
@@ -26,7 +26,7 @@ foreach($pages as $key => $title) {
   echo sprintf('<li><a href="%s">%s</a></li>', ROOT_URI.$key, $title);
 }
 ?>
-      <li><a href="https://meals.ga.ntu.edu.tw/meals_checksys/index.asp">填報-衛生檢查表</a></li>
+      <li><a href="https://meals.ga.ntu.edu.tw/tableware/">填報-衛生檢查表</a></li>
       <li><a href="https://meals.ga.ntu.edu.tw/tableware/">填報-餐具檢查</a></li>
     </ul>
   </li>
